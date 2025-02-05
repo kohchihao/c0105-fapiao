ffcdaf644531d1b076f3280183583a553c2d407d
Marcus Koh <kohchihao@gmail.com>
2025-02-05 22:29:35 +0800

    initial setup

---
+++ src/main.tsx
@@ -0,0 +1,15 @@
+import { MantineProvider } from '@mantine/core';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+
+import '@mantine/core/styles.css';
+import App from './App.tsx';
+import './index.css';
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <MantineProvider>
+      <App />
+    </MantineProvider>
+  </StrictMode>
+);

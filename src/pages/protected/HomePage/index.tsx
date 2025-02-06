f7d13fdacf716f42724977e3f3ee83b28dc25929
Marcus Koh <kohchihao@gmail.com>
2025-02-06 23:26:59 +0800

    feat: add logout

--- src/pages/protected/HomePage/index.tsx
+++ src/pages/protected/HomePage/index.tsx
@@ -1,7 +1,17 @@
+import { Button } from '@mantine/core';
+import supabase from '../../../utils/supabase';
+
 const HomePage = () => {
   return (
     <div>
       <h1> protected Home Page</h1>
+      <Button
+        onClick={() => {
+          supabase.auth.signOut();
+        }}
+      >
+        Logout
+      </Button>
     </div>
   );
 };
--- src/router/AuthProtectedRoute.tsx
+++ src/router/AuthProtectedRoute.tsx
@@ -1,12 +1,11 @@
-import { Outlet } from 'react-router-dom';
+import { Navigate, Outlet } from 'react-router-dom';
 import { useSession } from '../context/SessionContext';
-import NotFoundPage from '../pages/NotFoundPage';
 
 const AuthProtectedRoute = () => {
   const { session } = useSession();
   if (!session) {
     // or you can redirect to a different page and show a message
-    return <NotFoundPage />;
+    return <Navigate to="/" />;
   }
   return <Outlet />;
 };

649a41e6c0cb05c83e584d3beb46f66f0b1ae0f2
Marcus Koh <kohchihao@gmail.com>
2025-03-06 22:16:00 +0800

    feat: add logout function

--- src/components/NavbarMenu/index.tsx
+++ src/components/NavbarMenu/index.tsx
@@ -1,7 +1,20 @@
 import { Avatar, Menu } from '@mantine/core';
-import { IconSettings } from '@tabler/icons-react';
+import { notifications } from '@mantine/notifications';
+import { IconLogout2, IconSettings } from '@tabler/icons-react';
+import { ROUTES } from '../../constants';
+import supabase from '../../utils/supabase';
 
 const NavbarMenu = () => {
+  const handleLogout = async () => {
+    const { error } = await supabase.auth.signOut();
+    if (error) {
+      notifications.show({
+        color: 'red',
+        title: 'Error logging out',
+        message: error.message,
+      });
+    }
+  };
   return (
     <Menu shadow="md" width={200}>
       <Menu.Target>
@@ -12,10 +25,16 @@ const NavbarMenu = () => {
         <Menu.Label>Application</Menu.Label>
         <Menu.Item
           leftSection={<IconSettings size={14} />}
-          href="/app/settings"
+          href={ROUTES.SETTINGS_PAGE}
           component="a"
         >
           Settings
+        </Menu.Item>
+        <Menu.Item
+          leftSection={<IconLogout2 size={14} />}
+          onClick={handleLogout}
+        >
+          Logout
         </Menu.Item>
       </Menu.Dropdown>
     </Menu>

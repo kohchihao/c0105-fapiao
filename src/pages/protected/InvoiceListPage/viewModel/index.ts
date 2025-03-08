f5cc5a7356003a1c9df51d99160bc5ad1f982dee
Marcus Koh <kohchihao@gmail.com>
2025-03-08 14:54:50 +0800

    feat: impl search for invoices

--- src/pages/protected/InvoiceListPage/viewModel/index.ts
+++ src/pages/protected/InvoiceListPage/viewModel/index.ts
@@ -1,12 +1,16 @@
-import { useMemo } from 'react';
+import { useMemo, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import useAppNavigation from '../../../../hooks/useAppNavigation';
+import { useDebounce } from '../../../../hooks/useDebounce';
+import { useSearch } from '../../../../hooks/useSearch';
 import useInvoices from '../hooks/useInvoices';
 
 const useInvoiceListPageViewModel = () => {
   const { projectId: paramProjectId } = useParams();
   const { navigateCreateInvoicePage, navigateEditInvoicePage, navigateHome } =
     useAppNavigation();
+  const [query, setQuery] = useState('');
+  const debouncedQuery = useDebounce(query, 300);
 
   const projectId = useMemo(() => {
     return isNaN(Number(paramProjectId)) ? 0 : Number(paramProjectId);
@@ -19,6 +23,20 @@ const useInvoiceListPageViewModel = () => {
   } = useInvoices({
     projectId,
   });
+
+  const results = useSearch(
+    invoices,
+    ['invoice_sn', 'description'],
+    debouncedQuery
+  );
+
+  const onSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setQuery(event.currentTarget.value);
+  };
+
+  const onClearSearch = () => {
+    setQuery('');
+  };
 
   const onCreateInvoice = () => {
     navigateCreateInvoicePage({ projectId: String(projectId) });
@@ -36,12 +54,15 @@ const useInvoiceListPageViewModel = () => {
   };
 
   return {
-    invoices,
+    invoices: results,
     isError,
     isLoading,
     onCreateInvoice,
     onClickInvoice,
     onBackClick,
+    onSearchChange,
+    onClearSearch,
+    query,
   };
 };
 
